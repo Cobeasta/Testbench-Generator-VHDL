fa105eeb1d47ab8a23d90f1738891860cb5a656d
cmkonkol <cmkonkol@vlinux-14.ece.iastate.edu>
2023-02-01 12:34:38 -0600

    started working on UI for inputs

--- generate_tb.sh
+++ generate_tb.sh
@@ -4,6 +4,26 @@ AUTHOR=$(whiptail --inputbox "Enter author\'s name" 20 80 3>&1 1>&2 2>&3)
 DESCRIPTION=$(whiptail --inputbox "Enter Description" 20 80 3>&1 1>&2 2>&3)
 NOTES=$(whiptail --inputbox "Enter notes" 20 80 3>&1 1>&2 2>&3)
 
+
+# Loop for inputs:
+
+INPUTS=()
+SIGNALS=()
+EXIT_FLAG=0
+while [ $EXIT_FLAG=0 ]
+do
+	IN_NAME=$(whiptail --inputbox "Enter input name without \"i_\". Leave blank to exit" 20 80 3>&1 1>&2 2>&3)
+	if [ -z "$IN_NAME" ]
+	then
+		break
+
+	IN_DATATYPE=$(whiptail --inputbox "Enter datatype" 20 80 3>&1 1>&2 2>&3)
+	IN_DEFAULT=$(whiptail --inputbox "Enter default value in correct VHDL syntax" 20 80 3>&1 1>&2 2>&3)
+
+	$INPUT_LINE="i_${IN_NAME}: $IN_DATATYPE"
+	$SIGNAL_LINE="s_i${IN_NAME}: $IN_DATATYPE := $IN_DEFAULT"
+
+
 TESTBENCH_ENTITY_NAME="tb_"$COMP_NAME
 
 
